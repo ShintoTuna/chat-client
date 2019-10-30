52c96af603d5653b369eca6cd124197ebd6d84e1
Nikita Shatunov <nikita.shatunov@gmail.com>
2019-10-31 00:00:51 +0200

    fix bottom scroll messages

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, FC } from 'react';
+import React, { useContext, FC, useRef, useEffect } from 'react';
 import { ChatContext } from './ChatContext';
 import Message from './Message';
 import InputWithSubmit from './InputWithSubmit';
@@ -6,6 +6,17 @@ import { toHue, escapeHTMLEncode } from '../utils';
 
 const Chat: FC = () => {
     const { messages, sendMessage, username, disconnect, onlineUsers } = useContext(ChatContext);
+    const containerRef = useRef<HTMLDivElement>(null);
+
+    const keepAtBottom = () => {
+        const el = containerRef.current;
+
+        if (el) {
+            setTimeout(() => {
+                el.scrollTo(0, el.scrollHeight);
+            }, 0);
+        }
+    };
 
     const submit = (message: string) => {
         if (message.length > 0 && username) {
@@ -13,23 +24,27 @@ const Chat: FC = () => {
         }
     };
 
+    useEffect(() => {
+        keepAtBottom();
+    }, [messages]);
+
     return (
         <div className="chat">
             <button className="close" onClick={disconnect}>
                 ×
             </button>
-            <div className="messages">
+            <div className="users">
+                <h4>Online</h4>
+                <ul>
+                    {onlineUsers.map((user, i) => (
+                        <User key={i} user={user} />
+                    ))}
+                </ul>
+            </div>
+            <div className="messages" ref={containerRef}>
                 {messages.map((m, i) => (
                     <Message message={m} key={i} />
                 ))}
-                <div className="users">
-                    <h4>Online</h4>
-                    <ul>
-                        {onlineUsers.map((user, i) => (
-                            <User key={i} user={user} />
-                        ))}
-                    </ul>
-                </div>
             </div>
             <div className="controls">
                 <InputWithSubmit submit={submit} placeholder="Type to chat" />
