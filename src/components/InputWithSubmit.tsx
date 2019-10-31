e5bb76aa590bb9a37054423a6677139c2f1bc8df
Nikita Shatunov <nikita.shatunov@gmail.com>
2019-10-31 17:26:12 +0200

    cleanup

--- src/components/InputWithSubmit.tsx
+++ src/components/InputWithSubmit.tsx
@@ -45,7 +45,9 @@ const InputContainer = styled.div`
     flex-direction: row;
 `;
 
-const disabled = css`
+const common = css`
+    background-color: ${({ theme }) => theme.colors.lightBg};
+    border: none;
     :disabled {
         cursor: not-allowed;
     }
@@ -56,20 +58,16 @@ const Input = styled.input`
     display: inline-block;
     padding: 10px 15px;
     font-size: 18px;
-    border: none;
     border-radius: 5px 0 0 5px;
-    background-color: ${({ theme }) => theme.colors.lightBg};
     color: ${({ theme }) => theme.colors.lightFont};
     &::placeholder {
         color: ${({ theme }) => theme.colors.darkFont};
     }
-    ${disabled}
+    ${common}
 `;
 
 const Button = styled.button`
-    border: none;
     border-radius: 0 5px 5px 0;
-    background-color: ${({ theme }) => theme.colors.lightBg};
     font-size: 20px;
     padding: 15px;
     color: ${({ theme }) => theme.colors.darkFont};
@@ -77,7 +75,7 @@ const Button = styled.button`
     &::after {
         content: '➔';
     }
-    ${disabled}
+    ${common}
 `;
 
 export default InputWithSubmit;
